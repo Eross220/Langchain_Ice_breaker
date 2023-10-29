d52f21441e6b181305b281b8f84e6ef81ae5ce3c
Eross220 <blacktigerbusinesswork@gmail.com>
2023-10-28 23:50:12 -0400

    added agents

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,24 +1,29 @@
+from tools.tools import get_profile_url
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 
-from langchain.agents import initialize_agent , Tool , agent_types
+from langchain.agents import initialize_agent , Tool 
+from langchain.agents import AgentType
 
 def lookup(name: str) -> str:
     llm=ChatOpenAI(temperature=0,model="gpt-3.5-turbo")
-    template= """given the full name { name_of_person} I want you to get it me a link to their Linkedin profile page.
-                          Your answer should contain only a URL"""
-
+    template= """
+    given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+                          Your answer should contain only a URL
+    """
+    
     tools_for_agents=[
         Tool(
             name="Crawl Google for linkedin profile page",
-            func="?",
+            func=get_profile_url,
             description="useful for when you need get the linkedin Page URL",
             )
     ]
 
     agent= initialize_agent(
-        tools=tools_for_agents,llm=llm,
-        agent=agent_types.ZERO_SHOT_REACT_DESCRIPTION,
+        tools=tools_for_agents,
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True
     )
 
@@ -27,6 +32,7 @@ def lookup(name: str) -> str:
         input_variables=['name_of_person']
     )
 
-    linked_profile_url= agent.run(prompt_template.format_prompt(name_of_person=name))
 
+    linked_profile_url= agent.run(prompt_template.format_prompt(name_of_person=name))
+    
     return linked_profile_url
--- ice_breaker.py
+++ ice_breaker.py
@@ -3,12 +3,14 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from third_parties.linkedin import scrape_linkedin_profile
 
 
 if __name__ == '__main__':
     print("Hello Langchain!!")
 
-
+    linkedin_profile_url = linkedin_lookup_agent(name="Alexius xie")
     summmary_template= """
         given the Linkedin information {information} about a person from I want you to create:
         1.a short summary
@@ -22,7 +24,9 @@ if __name__ == '__main__':
 
     chain = LLMChain(llm=llm, prompt=summmary_prompt)
 
-    linkedin_data= scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118")
+    
+
+    linkedin_data= scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
     
     respons_llm=chain.run(information=linkedin_data)
 
